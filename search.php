7bbf7964ee94fbfa5471c3e5e46c380c4b183044
Shrelina Timilsina <shrelina@shrelinas-mbp.boisestate.edu>
2019-04-04 16:54:18 -0600

    done

---
+++ search.php
@@ -0,0 +1,20 @@
+<?php
+include 'header.php';
+?>
+
+<body>
+<Center>
+<h1>Search Results</h1>
+<img src ="s.png" alt="search" width= "150" height="150"/><b>
+<br>The results did not match any documents.</b>
+<h3>Suggestions:</h3>
+<ul>
+<li>Make sure all words are spelled correctly.</li>
+<li>Try different keywords.</li>
+</ul>
+</Center>
+</body>
+
+<?php
+include 'footer.php';
+?>
